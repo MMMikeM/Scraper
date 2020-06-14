268bbc5bde310ad70f1ee67f20db5be8f82026f5
Mike <mikem.tba@gmail.com>
2020-06-14 13:23:18 +0100

    working pushes

---
+++ main.js
@@ -0,0 +1,25 @@
+const cheerio = require('cheerio')
+const axios = require('axios')
+const url = 'https://kbdfans.com/collections/coming-soon/products/coming-soon-vulcan-x-kbdfans-maja-mechanical-keyboard-diy-kit'
+const PushBullet = require('pushbullet');
+const pusher = new PushBullet('o.L3hXHSFrLLXcwnwic31Ti7IkaHPXLVXF');
+
+
+
+axios.get(url).then((res) => {
+    let site = cheerio.load(res.data)
+    let text = site('#wait_li_register_trigger').text()
+    if (text !== 'Notify Me') {
+        pusher.devices('', function (error, response) {
+            console.log(response)
+            response.devices.map(x => {
+                pusher.note(x.iden, 'I am working', 'I can send pushes!!!!!', function (error, response) {
+                    console.log('success')
+                });
+            })
+        });
+
+    }
+})
+
+
